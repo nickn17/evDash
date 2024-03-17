1de6398c8499d035feffb1bb429375599416d102
Lubos Petrovic <nick.n17@gmail.com>
2024-03-17 06:56:12 +0100

    bin

--- update_bin.sh
+++ update_bin.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-rm dist/m5stack-core/evDash.ino.bin
 rm dist/m5stack-core2/evDash.ino.bin
-rm dist/ttgo-t4-v13/evDash.ino.bin
+rm dist/m5stack-cores3/evDash.ino.bin
 
-cp .pio/build/m5stack-core/firmware.bin dist/m5stack-core/evDash.ino.bin
 cp .pio/build/m5stack-core2/firmware.bin dist/m5stack-core2/evDash.ino.bin
-cp .pio/build/ttgo-t4-v13/firmware.bin dist/ttgo-t4-v13/evDash.ino.bin
+cp .pio/build/m5stack-cores3/firmware.bin dist/m5stack-cores3/evDash.ino.bin
 
